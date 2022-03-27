60aac6f3184f12bdeaefcee39584fc9fe77f4863
SVNKVN9 <72693906+SVNKVN9@users.noreply.github.com>
2022-03-27 12:46:45 +0700

    Update event.js

--- handlers/event.js
+++ handlers/event.js
@@ -1,12 +1,13 @@
 const { readdirSync } =  require('fs')
 
 module.exports = async (client) => {
-    ["client", "guild"].forEach(dir => {
+    const dirs = readdirSync('./event')
+    for(let dir of dirs) {
         const evnetFiles = readdirSync(`./event/${dir}`).filter(file => file.endsWith(".js"))
         for (const file of evnetFiles) {
             const evnet = require(`../event/${dir}/${file}`)
             let eventName = file.split(".")[0];
-            await client.on(eventName, evnet.bind(null, client))
+            client.on(eventName, evnet.bind(null, client))
         }
-    })
+    }
 } 
